3e5c3e9aac3d7ec1c15b213d7deb4ce7b06da388
MalihaAzmat10 <160611512+MalihaAzmat10@users.noreply.github.com>
2024-12-17 14:52:34 +0500

    User.java

---
+++ User.java
@@ -0,0 +1,12 @@
+package com.example.fyp;
+
+public class User {
+    private int id;
+    private String name;
+    private String email;
+
+    // Getters
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getEmail() { return email; }
+}
