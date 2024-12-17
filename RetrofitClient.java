039bcd6b9dde3df03fb4cec03367a48e1f5fb1bf
MalihaAzmat10 <160611512+MalihaAzmat10@users.noreply.github.com>
2024-12-17 14:51:19 +0500

    Create RetrofitClient.java

---
+++ RetrofitClient.java
@@ -0,0 +1,19 @@
+package com.example.fyp;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient() {
+        String baseUrl = "https://8b1a-111-88-87-233.ngrok-free.app//";// Update this line
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(baseUrl)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
